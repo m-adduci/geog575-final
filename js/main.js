d3aefecf256744807c2674f15db5c0e53b7e51a9
m-adduci <64570701+m-adduci@users.noreply.github.com>
2023-05-04 22:17:01 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ function main(){
   // set up the map center and zoom level
 var map = L.map('map', {
   center: [44.82, -85.1550],
-  zoom: 5,
+  zoom: 8,
   zoomControl: true,
   scrollWheelZoom: true
 });
